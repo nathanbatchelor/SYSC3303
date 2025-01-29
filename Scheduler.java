6ef112749ead80c48af62cc06a600d6d01e3073e
jandrews <jandrewskenora@gmail.com>
2025-01-29 17:35:16 -0500

    Updated Scheduler

--- Scheduler.java
+++ Scheduler.java
@@ -8,15 +8,19 @@ import java.util.Queue;
  *
  * @author Joey Andrwes
  * @author Grant Phillips
+ *
+ * @version 1.0
  */
 
 public class Scheduler implements Runnable {
 
     // Queue to hold FireEvents?
     private final Queue<FireEvent> queue = new LinkedList<>();
+    // Something to store zones
+    private final Map<Integer, FireIncidentSubsystem> zones = new HashMap<>();
+    // File for zones
+    private final String zoneFile;
 
-    // Hashmap to store zones?
-    // private final HashMap<Integer, Zone> zones = new HashMap<>();
 
     private volatile boolean isFinished = false;
 
@@ -25,26 +29,36 @@ public class Scheduler implements Runnable {
     }
 
     // Add fire events to queue
+    // Would FIS call: scheduler.addFireEvent(event);
     public synchronized void addFireEvent(FireEvent event) {
         queue.add(event);
         notifyAll();
     }
 
-    // get next fire event
+    /**
+     *
+     * @return The first FireEvent in the queue
+     */
+    // Would DS call: scheduler.getNextFireEvent();
     public synchronized FireEvent getNextFireEvent() {
         // get FireEvents from queue
+        while (queue.isEmpty() && !isFinished) {
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+
+        }
+        // return first event. If fire isn't put out, send another drone.
+        // Call FIS to see if the fire is out. If so, delete event from queue
+        return queue.peek();
     }
 
-    // Load zone details from file
-    private void loadZoneDetails(String zoneFile) {
-        // Use buffered reader to get file
-        // split up lines into zoneId and coordinates
-        // Add zone class to create zones from file?
-    }
-
-    // Potiential function to get a zone by zone id (if we use a zone class)
-    public synchronized Zone getZone(int zoneId){
-        return zones.get(zoneId);
+    // The FIS could call this when fire is extinguished?
+    // FIS has a count, counts number of litres dropped by drone?
+    // When count == int severity return true?
+    public synchronized void markFireExtinguished(FireEvent event) {
+        queue.remove(event);
+        System.out.println("Fire at Zone: " + event.getZoneId() + " Extinguished");
     }
 
     // Signal when all fires from input file are finished?
