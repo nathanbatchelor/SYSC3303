2efb689163902d5e9b665bced06d169aef2b27cc
Benradley <114306063+Benradley@users.noreply.github.com>
2025-02-01 19:50:38 -0500

    Add files via upload

---
+++ DroneTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class DroneTest {
+    /**
+     * Tests if the Drone runs for the expected time when running a set single event.
+     */
+    @Test
+    void testDroneCommunications() throws InterruptedException {
+        Scheduler scheduler = new Scheduler("src\\test_zone_file.csv", "src\\test_event_file.csv");
+        Thread drone = new Thread(new DroneSubsystem(scheduler));
+        drone.start();
+        Thread.sleep(1000);
+        assertTrue(drone.isAlive());
+        Thread.sleep(40000);
+        assertTrue(drone.isAlive());
+        Thread.sleep(40000);
+        assertTrue(!drone.isAlive());
+    }
+}
