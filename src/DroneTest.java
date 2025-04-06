2d3b8ccb3608891d24fec42dae7577334583e506
jasjotgill <jasjotgill@icloud.com>
2025-04-06 19:36:38 -0400

    Working Drone UDP Test

--- src/DroneTest.java
+++ src/DroneTest.java
@@ -1,22 +1,57 @@
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
-//
-//
-//public class DroneTest {
-//    /**
-//     * Tests if the Drone runs for the expected time when running a set single event.
-//     */
-//    @Test
-//    void testDroneCommunications() throws InterruptedException {
-//        Scheduler scheduler = new Scheduler("input//test_zone_file.csv", "input//test_event_file.csv");
-//        Thread drone = new Thread(new DroneSubsystem(scheduler));
-//        drone.start();
-//
-//        Thread.sleep(1000);
-//        assertTrue(drone.isAlive());
-//        Thread.sleep(40000);
-//        assertTrue(drone.isAlive());
-//        Thread.sleep(40000);
-//        assertFalse(drone.isAlive());
-//    }
-//}
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.io.*;
+import java.net.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DroneTest {
+
+    private static Scheduler scheduler;
+    private static int port = 5500;
+    private static int idnum;
+    private static InetAddress localhost;
+    private static DatagramSocket socket;
+
+    @BeforeAll
+    public static void setUpOnce() throws UnknownHostException, SocketException {
+        MetricsLogger logger = new MetricsLogger();
+        MapUI mapUI = new MapUI();
+        String fireIncidentFile = "src//input//test_event_file.csv";
+        String zoneFile = "src//input//test_zone_file.csv";
+        scheduler = new Scheduler(zoneFile, fireIncidentFile, 2, 200, mapUI, logger);
+        localhost = InetAddress.getLocalHost();
+        socket = new DatagramSocket(port);
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        if (socket != null && !socket.isClosed()) {
+            socket.close();
+        }
+    }
+
+    @Test
+    public void testDroneRPCSend()  {
+        try {
+            String response = "TEST";
+            InetAddress address = InetAddress.getLocalHost();
+            scheduler.droneRPCSend(response, 0);
+
+            byte[] buffer = new byte[4096];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            // Obtain the data from the packet
+            ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(packet.getData(), 0, packet.getLength()));
+            Object data = objectInputStream.readObject();
+
+            assertEquals("TEST", data);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
